30a7eace8cd63afeae7cc4a3e7a452721fe4abea
devaku-desktop <alejokimuy@gmail.com>
2020-05-03 11:14:06 +0800

    Added botsetup

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ if (
 }
 
 // Initialize the Viber Bot
-let bot = require('./lib_modules/viber/botsetup.js');
 let SERVER_URL = process.env.SERVER_URL;
+let bot = require('./lib_modules/viber/botsetup.js');
 
 // Initialize Express
 const expresssetup = require('./lib_modules/utils/expressetup.js');
@@ -23,6 +23,7 @@ app.listen(PORT, async function () {
         const ngrok = require('./lib_modules/utils/ngrokmodule.js');
         SERVER_URL = await ngrok.GetNgrokUrl();
     }
+    // Attach the Webhook
     bot.setWebhook(SERVER_URL);
     console.log(`Server is running at URL ${SERVER_URL}`);
 });
--- lib_modules/viber/botsetup.js
+++ lib_modules/viber/botsetup.js
@@ -1,5 +1,7 @@
 // Viber related
 const ViberBot = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
+const TextMessage = require('viber-bot').Message.Text;
 
 // Creating the bot with access token, name and avatar
 const bot = new ViberBot({
@@ -7,5 +9,51 @@ const bot = new ViberBot({
     name: process.env.VIBER_APP_TITLE,
     avatar: process.env.AVATAR,
 });
+
+// The user will get those messages on first registration
+bot.onSubscribe((response) => {
+    say(
+        response,
+        `Hi there ${response.userProfile.name}. I am ${bot.name}! This application was created by Alejo Kim Uy.`
+    );
+});
+
+bot.on(BotEvents.MESSAGE_RECEIVED, async function (message, response) {
+    // This bot will respond only to text messages
+    if (!(message instanceof TextMessage)) {
+        say(response, `Sorry, I only respond to text messages`);
+    } else {
+        console.log(message);
+        let { text } = message;
+        if (text.toLowerCase().trim() === 'about') {
+            SayAbout(response);
+        } else {
+            say(response, `You said: ${message.text}`);
+        }
+    }
+});
+
+function say(response, message) {
+    response.send(new TextMessage(message));
+}
+
+function SayAbout(response) {
+    let messageArray = [
+        `Hi there ${response.userProfile.name}. I am ${bot.name}! This Node.js application was created by Alejo Kim Uy.`,
+        `If you're interested in working with me, you can find my details on my LinkedIn: https://www.linkedin.com/in/alejo-kim-uy-612319108/`,
+        `If you want to see my other coding projects, you can find them on my Github: https://www.linkedin.com/in/alejo-kim-uy-612319108/`,
+    ];
+    let counter = 0;
+    let message = '';
+    let interval = setInterval(() => {
+        if (counter === messageArray.length) {
+            clearInterval(interval);
+        } else {
+            message = messageArray[counter];
+            say(response, message);
+            counter++;
+        }
+    }, 1000);
+}
 
 module.exports = bot;
